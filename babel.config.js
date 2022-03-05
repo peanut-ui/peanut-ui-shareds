e458a04a83e976c700d181c2e76253025a8617a3
Dirga Prakesha <dirga.prakesha@gmail.com>
2022-03-05 10:45:50 +0700

    chore: add babel minify and babel setup

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
 
-  const presets = ['@babel/preset-env']
-  const plugins = [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-    ['@babel/plugin-proposal-optional-chaining', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-  ]
+  const presets = ['@babel/preset-env', 'minify']
 
-  return {
-    presets,
-    plugins,
-  }
+  return { presets }
 }
